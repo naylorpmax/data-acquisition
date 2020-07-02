a3cc443d508ff4bd313bed19644f8f9df65c4b48
naylorpmax <nmax7515@gmail.com>
2020-07-02 08:24:45 -0400

    Speedup + error handling + cleanup

--- acquisition/__main__.py
+++ acquisition/__main__.py
@@ -7,63 +7,58 @@ import networkx as nx
 import pandas as pd
 
 
-# def main():
-# 	with open("testing.json") as f:
-# 		network = nx.readwrite.json_graph.adjacency_graph(f.read())
-	# print(network)
-
 def read_lines(path: str) -> List[Tuple[str, str]]:
 	with open(path) as f:
 		lines = f.readlines()
 	return [(line.split("|")[0].strip(), line.split("|")[1].strip()) for line in lines]
 
-def main():
-	playlist_names = read_lines(os.environ["PLAYLIST_PATH"])
 
+def main():
 	network = Network(
 		audio_features=["danceability", "energy", "key", "loudness", "mode", "speechiness", "acousticness", "instrumentalness", "liveness", "valence", "tempo", "duration_ms", "time_signature"],
 		max_tracks=50
 	)
 
+	playlist_names = read_lines(os.environ["PLAYLIST_PATH"])
 	for name, playlist_id in playlist_names:
 		start_time = datetime.datetime.now()
 		playlist = network.get_playlist(playlist_id=playlist_id)
 		print(f"Exploring via playlist: {playlist.name} ({playlist.id})")
-		add_artists(network, playlist.get_artists(), 0, 2, 15)
+		add_artists(network, playlist.get_artists(), 0, 2)
 
 		V, E = network.to_dataframe()
 
 		print(f"Writing {len(network.graph.nodes())} nodes and {len(network.graph.edges())} edges to file")
-		V.to_csv(f"vertices_{playlist.name}.csv", index=False)
-		E.to_csv(f"edges_{playlist.name}.csv", index=False)
+		V.to_csv(f"vertices_{name}.csv", index=False)
+		E.to_csv(f"edges_{name}.csv", index=False)
 		print(f"Completed: {datetime.datetime.now() - start_time}")
 
 
-def add_artists(network: Network, seed_artists: List[Artist], curr_depth: int, max_depth: int, max_breadth: int):
+def add_artists(network: Network, seed_artists: List[Artist], curr_depth: int, max_depth: int):
 	if curr_depth == max_depth or len(seed_artists) <= 0:
 		return
 
-	print(f"{len(seed_artists)} unseen artists in layer {curr_depth} of network")
+	unseen_seed_artists = network.unseen_artists(seed_artists)
+	print(f"{len(unseen_seed_artists)} unseen artists in layer {curr_depth} of network")
 	
-	associated_artists = []
-	for seed_artist in seed_artists:
+	next_seed_artists = []
+	for seed_artist in unseen_seed_artists:
 		found_tracks = network.search_tracks(seed_artist.name)
-		top_tracks = network.top_tracks(seed_artist)
+		top_tracks = network.top_tracks(seed_artist, True)
+		network.get_audio_features([tup[0] for tup in found_tracks + top_tracks])
+
 		related_artists = network.related_artists(seed_artist)
 		collaborators = {}
-
-		print(f"Added {len(found_tracks + top_tracks)} tracks by artist: {seed_artist.name}")
-
-		for retrieved_track, retrieved_artists in found_tracks + top_tracks:
-			network.put_track(retrieved_track, retrieved_artists)
-			for artist in retrieved_artists:
+		for track, artists in found_tracks + top_tracks:
+			network.put_track(track, artists)
+			for artist in artists:
 				collaborators[artist.name] = artist
 
-		associated_artists += list(set(related_artists + list(collaborators.values())))[:max_breadth]
+		unseen_associated_artists = network.unseen_artists(related_artists + list(collaborators.values()))
+		next_seed_artists += unseen_associated_artists
+		print(f"Tracks: {len(found_tracks + top_tracks)}\tAssociated artists: {len(unseen_associated_artists)}\tArtist: {seed_artist.name}")
 
-	known_artists = network.graph.nodes()
-	unknown_associated_artists = [artist for artist in list(set(associated_artists)) if artist.id not in known_artists]
-	add_artists(network, unknown_associated_artists, curr_depth+1, max_depth, max_breadth)
+	add_artists(network, next_seed_artists, curr_depth+1, max_depth)
 
 
 if __name__ == '__main__':
--- acquisition/spotifynetwork.py
+++ acquisition/spotifynetwork.py
@@ -15,8 +15,14 @@ class Track:
 		self.attr = attr
 		self.node_type = 'track'
 
-	def add_attrs(self, attrs: Dict[str, Any]):
-		self.attr.update(attrs)
+	def set_attrs(self, attrs: Dict[str, Any]):
+		self.attr = attrs
+
+	def __eq__(self, other):
+		return self.id == other.id and self.name == other.name and self.album == other.album and self.album_type == other.album_type
+
+	def __hash__(self):
+	    return hash(('id', self.id, 'name', self.name))
 
 
 class Artist:
@@ -27,7 +33,7 @@ class Artist:
 		self.node_type = 'artist'
 
 	def __eq__(self, other):
-		return self.id == other.id and self.name == other.name and self.attr == other.attr
+		return self.id == other.id and self.name == other.name
 
 	def __hash__(self):
 	    return hash(('id', self.id, 'name', self.name))
@@ -52,15 +58,16 @@ class Network:
 		self.audio_features = audio_features
 		self.max_tracks = max_tracks
 
-	def search_tracks(self, artist_name: str) -> List[Tuple[Track, List[Artist]]]:
+	def search_tracks(self, artist_name: str, seen: bool=False) -> List[Tuple[Track, List[Artist]]]:
 		tracks = {}
 		limit = 50; offset = 0; total = 1
 		while(offset <= total and offset <= self.max_tracks):
 			try:
-				results = self.client.search(artist_name, type="track", limit=limit, offset=offset)
+				results = self.client.search(artist_name, type='track', limit=limit, offset=offset)
 			except Exception as e:
-				print(f"Exception while searching, skipping remaining tracks:\n{e}")
-				break
+				print(f"exception while searching, skipping batch of tracks:\n{e}")
+				offset += limit
+				continue
 
 			offset += limit
 			total = results['tracks']['total']
@@ -68,96 +75,108 @@ class Network:
 			for result in results['tracks']['items']:
 				if artist_name in [artist['name'] for artist in result['artists']]:
 					tracks[result['name']] = (
-					 	Track(
-							id=result['id'],
-							name=result['name'],
-							album=result['album']['name'],
-							album_type=result['album']['album_type']
-						),
-						[Artist(
-							id=artist['id'],
-							name=artist['name']
-						) for artist in result['artists']]
+						self.track_from_response(result),
+						[self.artist_from_response(artist, seen) for artist in result['artists']]
 					)
 
 		return list(tracks.values())
 
-	def top_tracks(self, artist: Artist) -> List[Tuple[Track, List[Artist]]]:
-		results = self.client.artist_top_tracks(artist.id)
+	def top_tracks(self, artist: Artist, seen: bool=False) -> List[Tuple[Track, List[Artist]]]:
+		try:
+			results = self.client.artist_top_tracks(artist.id)
+		except Exception as e:
+			print(f"exception while getting top tracks, skipping artist ({artist.name}):\n{e}")
+			return []
+
 		tracks = {}
 		for result in results['tracks']:
 			tracks[result['name']] = (
-				Track(
-					id=result['id'],
-					name=result['name'],
-					album=result['album']['name'],
-					album_type=result['album']['album_type']
-				),
-				[Artist(
-					id=artist['id'],
-					name=artist['name']
-				) for artist in result['artists']]
+				self.track_from_response(result),
+				[self.artist_from_response(artist, seen) for artist in result['artists']]
 			)
 
 		return list(tracks.values())
 
 	def put_track(self, track: Track, artists: List[Artist]):
 		if track.id not in self.graph.nodes():
-			self.add_audio_features(track)
 			self.graph.add_node(track.id, track=track)
 			for artist in artists:
-				if artist.id not in self.graph.nodes():
+				seen = artist.attr.get('seen')
+				if seen:
+					self.graph.add_node(artist.id, artist=artist, seen=seen)
+				else:
 					self.graph.add_node(artist.id, artist=artist)
 				self.graph.add_edge(track.id, artist.id)
 
-	def related_artists(self, artist: Artist) -> List[Artist]:
-		results = self.client.artist_related_artists(artist.id)
+	def related_artists(self, artist: Artist, seen: bool=False) -> List[Artist]:
+		try:
+			results = self.client.artist_related_artists(artist.id)
+		except Exception as e:
+			print(f"exception while getting related artists, skipping artist ({artist.name}):\n{e}")
+			return []
+
 		if results['artists']:
-			return [Artist(
-				id=artist['id'],
-				name=artist['name']
-			) for artist in results['artists']]
+			return [self.artist_from_response(artist, seen) for artist in results['artists']]
 		else:
 			return []
 
-	def add_audio_features(self, track: Track) -> Track:
-		results = self.client.audio_features(track.id)
-		if len(results) <= 0 or not results[0]:
-			return
-		result = results[0]					# for now take the first one
-		assert(result['id'] == track.id)	# checking just in case
+	def get_audio_features(self, tracks: List[Track]):
+		n = 100
+		batches = [tracks[i * n:(i+1) * n] for i in range((len(tracks) + n-1) // n)]
 
-		audio_features = {}
-		for name in self.audio_features:
-			audio_features[name] = result.get(name)
-		track.add_attrs(audio_features)
+		for batch in batches:
+			tracks_map = {}
+			for track in batch:
+				tracks_map[track.id] = track
+
+			try:
+				results = self.client.audio_features(tracks_map.keys())
+			except Exception as e:
+				print(f"exception while getting audio features, skipping batch of tracks:\n{e}")
+				continue
+
+			if len(results) <= 0:
+				continue
+
+			for result in results:
+				if result:
+					track_id = result['id']
+					if track_id in tracks_map:
+						audio_features = {}
+						for name in self.audio_features:
+							audio_features[name] = result.get(name)
+						tracks_map[track_id].set_attrs(audio_features)
 
 	def get_playlist(self, playlist_id: str) -> Playlist:
-		playlist = self.client.playlist(playlist_id=playlist_id)
+		try:
+			playlist = self.client.playlist(playlist_id=playlist_id)
+		except Exception as e:
+			print(f"exception while getting playlist, skipping playlist ({playlist_id}):\n{e}")
+			return Playlist(id='', name='', entries=[])
+
 		playlist_name = playlist['name']
 		total_tracks = playlist['tracks']['total']
 
 		entries = {}; offset = 0; limit = 50
 		while(offset <= total_tracks):
-			results = self.client.playlist_tracks(playlist_id=playlist_id, offset=offset, limit=limit)
+			try:
+				results = self.client.playlist_tracks(playlist_id=playlist_id, offset=offset, limit=limit)
+			except Exception as e:
+				print(f"exception while getting playlist tracks, skipping batch of tracks ({playlist_id}):\n{e}")
+				offset += limit
+				continue
+
 			offset += limit
 			if len(results['items']) <= 0 or not results['items'][0]:
 				break
 
 			for result in results['items']:
-				track = result['track']
-				entries[track['name']] = (
-					Track(
-						id=track['id'],
-						name=track['name'],
-						album=track['album']['name'],
-						album_type=track['album']['album_type']
-					),
-					[Artist(
-						id=artist['id'],
-						name=artist['name']
-					) for artist in track['artists']]
-				)
+				if result['track']:
+					track = result['track']
+					entries[track['name']] = (
+						self.track_from_response(track),
+						[self.artist_from_response(artist) for artist in track['artists']]
+					)
 
 		return Playlist(
 			id=playlist_id,
@@ -166,6 +185,7 @@ class Network:
 		)			
 
 	def to_dataframe(self) -> (pd.DataFrame, pd.DataFrame):
+		artists = nx.get_node_attributes(self.graph, 'artist')
 		tracks = [track.__dict__ for track in nx.get_node_attributes(self.graph, 'track').values()]
 		artists = [artist.__dict__ for artist in nx.get_node_attributes(self.graph, 'artist').values()]
 		V = pd.json_normalize(tracks + artists)
@@ -186,7 +206,32 @@ class Network:
 			elif record['node_type'] == 'artist':
 				G.add_node(record['id'], artist=Artist(id=record['id'], name=record['name']))
 			else:
-				print("Weird node found; skipping")
+				print("weird node found; skipping")
 				continue
 
 		self.graph = G
+
+	def unseen_artists(self, artists: List[Artist]):
+		unique_artists = list(set(artists))
+		seen_artists = nx.get_node_attributes(self.graph, 'seen')
+		return [artist for artist in unique_artists if artist.id not in seen_artists]
+
+	def artist_from_response(self, response: Dict[str, Any], seen: bool=False) -> Artist:
+		attr = {
+			'popularity': response.get('popularity'),
+			'genres': response.get('genres')
+		}
+
+		if seen or response['id'] in nx.get_node_attributes(self.graph, 'seen'):
+			attr['seen'] = True
+
+		artist = Artist(id=response['id'], name=response['name'], attr=attr)
+		return artist
+
+	def track_from_response(self, response: Dict[str, Any]) -> Track:
+		return Track(
+			id=response['id'],
+			name=response['name'],
+			album=response['album']['name'],
+			album_type=response['album']['album_type']
+		)
