cce7d1db429f415b26430ecd8b679364f3e0f8ae
naylorpmax <67208755+naylorpmax@users.noreply.github.com>
2020-08-30 18:21:11 -0400

    Collect music data via Spotify API + LastFM API (#1)
    * Create spotifynetwork library
    
    * Recursive playlist search
    
    * Persist vertex and edge lists as csv files
    
    * Speedup + error handling + cleanup
    
    * Async LastFM client to retrieve artist tags

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # data-acquisition 
+
+Python library to collect artist and track data by leveraging networkx, spotipy, and the LastFM API.
+
+```
+# install dependencies
+pip3 install -r requirements.txt
+
+# configure
+export PLAYLISTS_PATH=acquisitions/playlists.txt
+export SPOTIPY_CLIENT_ID=<spotify-client-id>
+export SPOTIPY_CLIENT_SECRET=<spotify-client-secret>
+export LASTFM_CLIENT_ID=<lastfm-client-id>
+export LASTFM_CLIENT_SECRET=<lastfm-client-secret>
+
+# run library
+python3 -m acquisition
+
+# tag artists
+python3 acquisition/tag.py
+```
---
+++ acquisition/__main__.py
@@ -0,0 +1,77 @@
+import json, os, time
+from typing import List, Tuple
+
+from .spotifynetwork import Track, Network, Artist
+
+import networkx as nx
+import pandas as pd
+
+
+def main():
+	network = Network(
+		audio_features=["danceability", "energy", "key", "loudness", "mode", "speechiness", "acousticness", "instrumentalness", "liveness", "valence", "tempo", "duration_ms", "time_signature"],
+		max_tracks=50
+	)
+
+	vertices_path = os.environ.get("VERTICES_PATH")
+	edges_path = os.environ.get("EDGES_PATH")
+	playlists_path = os.environ.get("PLAYLISTS_PATH")
+
+	if vertices_path and edges_path:
+		print("Loading graph from file")
+		V = pd.read_csv(vertices_path)
+		E = pd.read_csv(edges_path)
+		network.from_dataframe(V, E)
+		print(f"Loaded {len(network.graph.nodes)} nodes and {len(network.graph.edges)}")
+
+	print("Adding new tracks and artists via seed playlists")
+	playlist_names = read_lines(playlists_path)
+	for name, playlist_id in playlist_names:
+		start_time = time.time()
+		playlist = network.get_playlist(playlist_id=playlist_id)
+		print(f"Exploring via playlist: {playlist.name} ({playlist.id})")
+		add_artists(network, playlist.get_artists(), 0, 2)
+
+		V, E = network.to_dataframe()
+
+		print(f"Writing {len(network.graph.nodes())} nodes and {len(network.graph.edges())} edges to file")
+		V.to_csv(f"vertices_{name}.csv", index=False)
+		E.to_csv(f"edges_{name}.csv", index=False)
+		print(f"Completed: {time.time() - start_time}")
+
+
+def add_artists(network: Network, seed_artists: List[Artist], curr_depth: int, max_depth: int):
+	if curr_depth == max_depth or len(seed_artists) <= 0:
+		return
+
+	unseen_seed_artists = network.unseen_artists(seed_artists)
+	print(f"{len(unseen_seed_artists)} unseen artists in layer {curr_depth} of network")
+	
+	next_seed_artists = []
+	for seed_artist in unseen_seed_artists:
+		found_tracks = network.search_tracks(seed_artist.name)
+		top_tracks = network.top_tracks(seed_artist, True)
+		network.get_audio_features([tup[0] for tup in found_tracks + top_tracks])
+
+		related_artists = network.related_artists(seed_artist)
+		collaborators = {}
+		for track, artists in found_tracks + top_tracks:
+			network.put_track(track, artists)
+			for artist in artists:
+				collaborators[artist.name] = artist
+
+		unseen_associated_artists = network.unseen_artists(related_artists + list(collaborators.values()))
+		next_seed_artists += unseen_associated_artists
+		print(f"Tracks: {len(found_tracks + top_tracks)}\tAssociated artists: {len(unseen_associated_artists)}\tArtist: {seed_artist.name}")
+
+	add_artists(network, next_seed_artists, curr_depth+1, max_depth)
+
+
+def read_lines(path: str) -> List[Tuple[str, str]]:
+	with open(path) as f:
+		lines = f.readlines()
+	return [(line.split("|")[0].strip(), line.split("|")[1].strip()) for line in lines]
+
+
+if __name__ == '__main__':
+	main()
